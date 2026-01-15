2491a67ed9651c0f840750ae75fb211548ba4aad
Breslavets Maria <alwayslearn@mail.ru>
2026-01-15 20:58:28 +0300

    feat: getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -229,8 +229,13 @@ function partialUsingArguments(fn, ...args) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom;
+  return function increase() {
+    const id = current;
+    current += 1;
+    return id;
+  };
 }
 
 module.exports = {
