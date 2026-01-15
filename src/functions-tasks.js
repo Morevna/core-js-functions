a7166da3bf29ac50621dc635b1b6be249f39b2af
Breslavets Maria <alwayslearn@mail.ru>
2026-01-15 17:30:47 +0300

    feat: solve memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -119,8 +119,16 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cached;
+  let called;
+  return function check() {
+    if (!called) {
+      cached = func();
+      called = true;
+    }
+    return cached;
+  };
 }
 
 /**
