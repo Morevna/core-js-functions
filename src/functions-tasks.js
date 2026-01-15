752d96a154b52ff5e44895c881a389e97f63b4dd
Breslavets Maria <alwayslearn@mail.ru>
2026-01-15 20:50:35 +0300

    feat: partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -206,8 +206,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return function add(...addArgs) {
+    return fn(...args, ...addArgs);
+  };
 }
 
 /**
