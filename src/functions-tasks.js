5d44297567b5e8d0cd48b4fb78e6f23335a84dbc
Breslavets Maria <alwayslearn@mail.ru>
2026-01-15 18:12:04 +0300

    feat: solve retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -146,8 +146,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function a() {
+    let lastErr;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        lastErr = err;
+      }
+    }
+    throw lastErr;
+  };
 }
 
 /**
